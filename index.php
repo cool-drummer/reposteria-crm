9fc21dd12d47b57cafd85ca0ffdb39eb086185a3
Dan Rojas <dan_irm2002@hotmail.com>
2025-06-22 19:07:48 -0600

    feat: new (index.php) add to estructure html

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-
-echo "Hello World";
+?>
+<header>
+    <h1>Hello World</h1>
+    <h2>Hello World</h2>
+    <h3>Hello World</h3>
+    <h4>Hello World</h4>
+    <h5>Hello World</h5>
+    <h6>Hello World</h6>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>  
+</header>
+<main>
+    <p>This is a test</p>
+</main>
+<footer>
+    <p>Copyright 2025</p>
+</header>
